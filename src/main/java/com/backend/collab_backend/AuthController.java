cdb049a2791f5b1866200d760085d858c7f4d742
agavrilita <agavrilita@inthergroup.com>
2023-05-15 00:10:19 +0300

    security_1

---
+++ src/main/java/com/backend/collab_backend/AuthController.java
@@ -0,0 +1,40 @@
+//*************************************************//
+//          INTHER LOGISTICS ENGINEERING           //
+//*************************************************//
+
+package com.backend.collab_backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+  @GetMapping("/login")
+  public String loginEndpoint() {
+    return "Login!";
+  }
+
+  @GetMapping("/admin")
+  public String adminEndpoint() {
+    return "Admin!";
+  }
+
+  @GetMapping("/user")
+  public String userEndpoint() {
+    return "User!";
+  }
+
+  @GetMapping("/all")
+  public String allRolesEndpoint() {
+    return "All Roles!";
+  }
+
+  @DeleteMapping("/delete")
+  public String deleteEndpoint(@RequestBody String s) {
+    return "I am deleting " + s;
+  }
+}
