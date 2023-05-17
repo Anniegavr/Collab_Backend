8732d0477ac6e9a248b7be79542ee213940e8ac7
agavrilita <agavrilita@inthergroup.com>
2023-05-17 22:25:45 +0300

    - Apply patch from local mock (faster) server

--- src/main/java/com/backend/collab_backend/student/StudentController.java
+++ src/main/java/com/backend/collab_backend/student/StudentController.java
@@ -1,6 +1,8 @@
 package com.backend.collab_backend.student;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,18 +21,19 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class StudentController {
-
+  private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
   private final StudentService studentService;
 
   @GetMapping
   public List<StudentDTO> getAllStudents() {
+    logger.info("Getting all students");
     return studentService.getAllStudents();
   }
 
   @GetMapping("/{id}")
   public StudentDTO getStudentById(@PathVariable Long id) {
+    logger.info("Getting student with ID: {}", id);
     return studentService.getStudentById(id);
   }
-
 
 }
--- src/main/java/com/backend/collab_backend/student/StudentService.java
+++ src/main/java/com/backend/collab_backend/student/StudentService.java
@@ -1,7 +1,3 @@
-//*************************************************//
-//          INTHER LOGISTICS ENGINEERING           //
-//*************************************************//
-
 package com.backend.collab_backend.student;
 
 import java.util.List;
