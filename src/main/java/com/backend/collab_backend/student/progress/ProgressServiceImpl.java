8e3dfe2a8de8c510cf6e0e2bb9d7e11e105c1429
agavrilita <agavrilita@inthergroup.com>
2023-06-08 11:15:33 +0300

    Backend-09 - Link the Student and the Assignment entities with the Progress (task progress) entity
    - Add record new progress functionality

--- src/main/java/com/backend/collab_backend/student/progress/ProgressServiceImpl.java
+++ src/main/java/com/backend/collab_backend/student/progress/ProgressServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +26,15 @@ public class ProgressServiceImpl implements ProgressService {
   }
 
   public ProgressDTO recordProgressForStudent(Long id, ProgressDTO progress) {
-    return null;
+    Optional<Progress> currentProgress = progressRepository.findProgressByTaskNameAndStudentId(progress.taskName, id);
+    if (currentProgress.isPresent()) {
+      logger.info("Recording progress: additional [{}] hours for task[{}]", progress.taskProgress, progress.taskName);
+      currentProgress.get().setTaskProgress(currentProgress.get().getTaskProgress() + progress.taskProgress);
+    } else {
+      logger.info("Couldn't find task[{}] to record the progress for.", progress.taskName);
+      progress.taskProgress = 0;
+      return progress;
+    }
+    return new ProgressDTO(currentProgress.get().getTaskName(), currentProgress.get().getTaskProgress());
   }
 }
